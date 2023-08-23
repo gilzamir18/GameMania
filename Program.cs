35733904cbaad4f1804efac6e30c2cedeea0de34
Gilzamir Gomes <54516662+gilzamir18@users.noreply.github.com>
2023-08-23 16:41:14 -0300

    Estrutura inicial do projeto.
    Adicionado um arquivo de gitignore.
    Adicionado um arquivo de projeto csharp do dotnet (csproj).
    Criado um arquivo principal de programa (Program.cs).

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿List<string> jogos = new List<string>();
+int opcao;
+do{
+
+    Console.WriteLine("-------------------- GAMEMANIA --------------------");
+    Console.WriteLine("1 - Cadastrar novo Jogo");
+    Console.WriteLine("2 - Exibir jogos cadastrados ");
+    Console.WriteLine("3 - Mostrar detalhes dos jogos");
+    Console.WriteLine("4 - Avaliar jogo");
+    Console.WriteLine("5 - Sair");
+
+    var input = Console.ReadLine();
+    opcao = int.Parse(input);
+
+    switch (opcao){
+        case 1:
+            Console.WriteLine("Informe o nome do jogo");
+            var nomeDoJogo = Console.ReadLine();
+            jogos.Add(nomeDoJogo);
+            Console.WriteLine("Jogo Adicionado com sucesso");
+            break;
+        case 2:
+            Console.WriteLine("Exibindo jogos cadastrados");
+            foreach (var jogo in jogos){
+                Console.WriteLine(jogo);
+            }
+            break;
+        case 3:
+            Console.WriteLine("Detalhes do jogo selecionado!");
+            break;
+        case 4:
+            Console.WriteLine("Avaliação do jogo selecionado!");
+            break;
+        case 5:
+            Console.WriteLine("Saindo...");
+            break;
+        default:
+            Console.WriteLine("Volte sempre!");
+            break;
+    }
+
+
+} while (opcao != 5);
