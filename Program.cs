b76136b74b878c3433390a848346b0f60ea4b6c5
Weslley Rufino <weslley.rufino@outlook.com>
2023-08-30 15:28:10 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
-﻿
+﻿Jogo jogo = new Jogo();
+jogo.Titulo = "FarCry";
 
-List<Jogo> listaJogos = new(){
-    new Jogo("FarCry","Acao/Aventura","Ubisoft","3","3edicao",true,"AllPlatform",notas:new List<float>{10,9,8}),
-    new Jogo("GTA","Acao/Aventura","RockStar","5","5edicao",true,"AllPlatform",notas:new List<float>{8,9,8})
-    };
 
-void ExibirMensagemBoasVindas(){
+//List<string> jogos = new() {"GTA", "Valorant", "CounterStrike", "NeedForSpeed"} ;
+/*
+Dictionary< string, List<int> > jogosRegistrados = new();
+jogosRegistrados["Forza"] = new List<int>(){10, 7, 8};
+jogosRegistrados["Valorant"] = new List<int>(){10, 5, 7}; 
+jogosRegistrados["FarCry"] = new List<int>{10, 10, 10};
+
+void ExibirMensagemBoasVindas()
+{
     Console.WriteLine(@"
 ░██████╗░░█████╗░███╗░░░███╗███████╗███╗░░░███╗░█████╗░███╗░░██╗██╗░█████╗░
 ██╔════╝░██╔══██╗████╗░████║██╔════╝████╗░████║██╔══██╗████╗░██║██║██╔══██╗
@@ -13,16 +18,30 @@ void ExibirMensagemBoasVindas(){
 ██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░██║╚██╔╝██║██╔══██║██║╚████║██║██╔══██║
 ╚██████╔╝██║░░██║██║░╚═╝░██║███████╗██║░╚═╝░██║██║░░██║██║░╚███║██║██║░░██║
 ░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝╚═╝░░╚═╝");
+    Console.WriteLine("Seja bem-vindo ao GAMEMANIA!");
 }
 
-void RodapeVoltarParaPrincipal(){
-    Console.WriteLine("Pressione Qualquer Tecla Para Voltar ao Menu Principal...");
+void RodapeVoltarParaPrincipal()
+{
+    Console.WriteLine("Pressione qualquer tecla para voltar ao menu principal...");
     Console.ReadKey();
+    //Thread.Sleep(1000);
     Console.Clear();
     MenuPrincipal();
 }
 
-void ExibirTituloDaOpcao(string titulo, char preencher='*'){
+void CadastrarNovoJogo()
+{
+
+    ExibirTituloDaOpcao("Cadastrar um novo Jogo");
+    var nomeDoJogo = Console.ReadLine();
+    jogosRegistrados.Add(nomeDoJogo, new List<int>());
+    Console.WriteLine("Jogo Adicionado com sucesso");
+    RodapeVoltarParaPrincipal();
+}
+
+void ExibirTituloDaOpcao(string titulo, char preencher='*')
+{
     Console.Clear();
     var barra = string.Empty.PadLeft(titulo.Length, preencher);
     Console.WriteLine(barra);
@@ -30,193 +49,66 @@ void ExibirTituloDaOpcao(string titulo, char preencher='*'){
     Console.WriteLine(barra);
 }
 
-Jogo? EncontrarJogo(string? titulo) {
-    if (string.IsNullOrEmpty(titulo)) {
-        return null;
-    }
-    foreach (Jogo jogo in listaJogos) {
-        if (jogo.Titulo == titulo) {
-            return jogo;
-        }
-    }
-    return null;
-}
-
-string? ForcedValidationString(string? aux){
-    while(string.IsNullOrEmpty(aux)) {
-        Console.Write("Tente Novamente: ");
-        aux = Console.ReadLine();
-    }
-    return aux;
-}
-
-float ForcedValidationFloat(string? aux) {
-    float floatValue;
-    while (!float.TryParse(aux, out floatValue)) {
-        Console.WriteLine("Tente Novamente: ");
-        aux = Console.ReadLine();
-    }
-    return floatValue;
-}
-
-void CadastrarNovoJogo(){
-    ExibirTituloDaOpcao("Cadastrar um novo Jogo - Digite -1 Para Cancelar");
-    Console.Write("Insira o Titulo do Jogo: ");
-    string? aux = Console.ReadLine();
-    while (string.IsNullOrEmpty(aux) || EncontrarJogo(aux) != null) {
-        Console.Write("Nome inválido ou já cadastrado. Tente Novamente: ");
-        aux = Console.ReadLine();
-    }
-    if(aux != "-1"){
-        Jogo? jogo = new(aux);
-        Console.WriteLine("Insira Genero: ");
-        aux = Console.ReadLine();
-        jogo.Genero = ForcedValidationString(aux);
-
-        Console.WriteLine("Insira Studio: ");
-        aux = Console.ReadLine();
-        jogo.Studio = ForcedValidationString(aux);        
-
-        Console.WriteLine("Insira Edicao: ");
-        aux = Console.ReadLine();
-        jogo.Edicao = ForcedValidationString(aux);
-
-        // ReadOnly
-        // Console.WriteLine("Insira Descricao: ");
-        // aux = Console.ReadLine();
-        // jogo.Descricao = ForcedValidationString(aux);
-
-        Console.WriteLine("Disponivel Para Avaliacao? (1 = Sim | 0 - Nao)");
-        aux = Console.ReadLine();
-        while( (aux != "0" && aux != "1") || string.IsNullOrEmpty(aux)){
-            Console.Write("Tente Novamente");
-            aux = Console.ReadLine();
-        }
-        jogo.Disponibilidade = (aux == "1")?true:false;
-
-        Console.WriteLine("Insira Plataformas: ");
-        aux = Console.ReadLine();
-        jogo.Plataformas = ForcedValidationString(aux);
-
-        List<float> notas = new();
-        Console.WriteLine("Insira Notas - Digite -1 Para Cancelar");
-        float n = 0;
-        while(n >= 0){
-            aux = Console.ReadLine();
-            n = ForcedValidationFloat(aux);
-            if(n>=0){
-                notas.Add(n);
-            }else{
-                n=-1;
-            }
-        }
-        jogo.Notas = notas;
-
-        listaJogos.Add(jogo);
-        Console.WriteLine("Jogo Adicionado Com Sucesso");        
-    }else{
-        Console.WriteLine("Cadastro Cancelado");
-    }
-    RodapeVoltarParaPrincipal();
-}
-
-void ExibirJogosCadastrados(){
+void ExibirJogosCadastrados()
+{
     ExibirTituloDaOpcao("Exibindo Jogos");
-    if(listaJogos.Count == 0){
-        Console.WriteLine("Nenhum Jogo Cadastrado!");
-    }else{
-        foreach (Jogo jogo in listaJogos){
-            Console.WriteLine(jogo.Titulo);
-        }
+    foreach (var jogo in jogosRegistrados.Keys)
+    {
+        var notas = jogosRegistrados[jogo];
+        Console.WriteLine($"Título: {jogo}");
     }
     RodapeVoltarParaPrincipal();
 }
 
-void ExibirDetalhesDoJogo(){
-    ExibirTituloDaOpcao("Exibindo Detalhes do Jogo - Digite -1 Para Cancelar Busca");
-    string? titulo = "0";
-    while(titulo != "-1"){
-        Console.Write("Informe o Título Do Jogo - Digite -1 Para Cancelar Busca: ");
-        titulo = Console.ReadLine();
-        titulo = (!string.IsNullOrWhiteSpace(titulo)) ? titulo : "";
-        if (titulo != "-1"){
-            Jogo? jogo = EncontrarJogo(titulo);
-            if(jogo==null){
-                Console.WriteLine("Titulo Nao Encontrado");
-            }else{
-                jogo.ExibirFichaTecnica();
-            }             
-        }  
+void ExibirDetalhesDoJogo()
+{
+    ExibirTituloDaOpcao("Exibindo Detalhes do Jogo");
+    Console.Write("Informe o título do jogo: ");
+    string titulo = Console.ReadLine();
+    if (jogosRegistrados.ContainsKey(titulo))
+    {
+        var notas = jogosRegistrados[titulo];
+        Console.WriteLine($"A média de avaliação do jogo {titulo} é: {notas.Average()}");
     }
-    MenuPrincipal();
+    else
+    {
+        Console.WriteLine($"Não existe um jogo com o título {titulo}");
+    }
 }
 
-void AvaliarJogoCadastrado(){
-    ExibirTituloDaOpcao("Qual Jogo Deseja Avaliar? - Digite -1 Para Cancelar");
-    Console.Write("Insira o Titulo do Jogo a Ser Avaliado: ");
-    string? aux = Console.ReadLine();
-    while (string.IsNullOrEmpty(aux) || EncontrarJogo(aux) == null) {
-        if(aux == "-1"){
-            RodapeVoltarParaPrincipal();
-        }
-        Console.Write("Nome inválido ou já cadastrado. Tente Novamente: ");
-        aux = Console.ReadLine();
-    }
-    Jogo? jogo = EncontrarJogo(aux);
-    if(jogo == null){//Apenas para tirar o warning
-        RodapeVoltarParaPrincipal();
-        jogo = (jogo == null) ? new() : jogo;
-    }
-    Console.WriteLine("Insira Notas - Digite -1 Para Cancelar");
-    float n = 0;
-    jogo.Notas = jogo.Notas == null ? new List<float>{0} : jogo.Notas; //Apenas para tirar o warning
-    while(n >= 0){
-        aux = Console.ReadLine();
-        n = ForcedValidationFloat(aux);
-        if(n>=0){   
-            jogo.Notas.Add(n);
-        }else{
-            n=-1;
-        }
-    }        
-    
-    RodapeVoltarParaPrincipal();
-}
-
-void MenuPrincipal(){
-    Console.Clear();
-    string? input;
+void MenuPrincipal()
+{
+    int opcao;
     ExibirMensagemBoasVindas();
-    Console.WriteLine("0 - Sair");
-    Console.WriteLine("1 - Cadastrar Novo Jogo");
-    Console.WriteLine("2 - Exibir Jogos Cadastrados ");
-    Console.WriteLine("3 - Mostrar Detalhes Dos Jogos");
-    Console.WriteLine("4 - Avaliar Jogo");
+    Console.WriteLine("1 - Cadastrar novo Jogo");
+    Console.WriteLine("2 - Exibir jogos cadastrados ");
+    Console.WriteLine("3 - Mostrar detalhes dos jogos");
+    Console.WriteLine("4 - Avaliar jogo");
+    Console.WriteLine("5 - Sair");
 
-    input = Console.ReadLine();
-    input = (!string.IsNullOrWhiteSpace(input))? input: "-1";
-    switch (input){
-        case "0":
-            Console.WriteLine("Volte sempre...");
-            return;
-        case "1":
+    var input = Console.ReadLine();
+    opcao = int.Parse(input);
+
+    switch (opcao){
+        case 1:
             CadastrarNovoJogo();
             break;
-        case "2":
+        case 2:
             ExibirJogosCadastrados();
             break;
-        case "3":
+        case 3:
             ExibirDetalhesDoJogo();
             break;
-        case "4":
-            AvaliarJogoCadastrado();
+        case 4:
+            Console.WriteLine("Avaliação do jogo selecionado!");
+            break;
+        case 5:
+            Console.WriteLine("Volte sempre...");
             break;
         default:
             Console.WriteLine("Opção inválida: tente outra opção!");
-            MenuPrincipal();
             break;
     }
-          
 }
 
-MenuPrincipal();
+MenuPrincipal();*/
