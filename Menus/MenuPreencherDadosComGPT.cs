b38f4417c7c003fcddf5f26e3fa4a4b854573900
Weslley Rufino <weslley.rufino@outlook.com>
2023-09-03 02:03:01 -0300

    MenuPreencherDadosComGPT V1
    Adição de informações básicas está funcionando

--- Menus/MenuPreencherDadosComGPT.cs
+++ Menus/MenuPreencherDadosComGPT.cs
@@ -7,42 +7,59 @@ internal class MenuPreencherDadosComGPT: Menu{
 
     }    
     public override async Task ExecutarOpcao(Dictionary<string, Jogo> jogosRegistrados){
-            //await RequisitarAPI();
-            string jsonString = "";
-            // string jsonString = @"{
-            //     ""Genero"": ""Ação/Aventura"",
-            //     ""Studio"": ""Ubisoft"",
-            //     ""Edicao"": ""3"",
-            //     ""Descricao"": ""Bem-vindo à loucura tropical de Far Cry 3! Jogue como Jason Brody, um turista em uma ilha paradisíaca que se transforma em um pesadelo mortal. Lute contra piratas, explore selvas exuberantes e mergulhe em uma história cheia de reviravoltas e personagens inesquecíveis. Prepare-se para caos, explosões e muita diversão!"",
-            //     ""Disponibilidade"": true,
-            //     ""Plataformas"": ""PlayStation, Xbox, PC""
-            // }";
-            jsonString = await RequisitarAPI(titulo:"FarCry3");
+        Console.WriteLine("Cadastrar Um Novo Jogo Usando ChatGPT | Digite -1 Para Cancelar: ");
+        Console.Write("Titulo: ");
+        string? aux = Console.ReadLine();
+        aux = string.IsNullOrEmpty(aux) ? "" : aux;
+
+        while((string.IsNullOrEmpty(aux) || jogosRegistrados.ContainsKey(aux)) && aux != "-1"){
+            Console.Write("Nome Invalido Ou Ja Cadastrado. Tente Novamente: ");
+            aux = Console.ReadLine();
+            aux = string.IsNullOrEmpty(aux) ? "" : aux;
+        }
+        if(aux != "-1"){        
+            string? jsonString = await RequisitarAPI(titulo:aux);
+            if(string.IsNullOrEmpty(jsonString)){
+                return;
+            }
             Jogo? jogo = JsonConvert.DeserializeObject<Jogo>(jsonString);
             jogo = jogo==null?new Jogo():jogo;
-            jogo.Titulo = "FarCry3";
-            jogo.ExibirFichaTecnica();
+            jogo.Titulo = aux;
+            jogo.Titulo = string.IsNullOrEmpty(jogo.Titulo)?"":jogo.Titulo;
             jogosRegistrados.Add(jogo.Titulo,jogo);
-
+            jogo.ExibirFichaTecnica();
+            Console.WriteLine("Jogo Adicionado Com Sucesso");        
+        }else{
+            Console.WriteLine("Cadastro Cancelado");
+        }
             await Task.Delay(0);
     }
     public virtual async Task<string> RequisitarAPI(string titulo){
-        var client = new OpenAI_API.OpenAIAPI(Environment.GetEnvironmentVariable("OPENAI_KEY"));
-        var chat = client.Chat.CreateConversation();
-        string prompt = $@"Para o jogo ""{titulo}"", preencha os campos adequadamente e retorne apenas o arquivo JSON preenchido. Descricao: Resuma o jogo de forma divertida e breve. Não repita informações de outros campos:
-        {{
-        ""Genero"": """",
-        ""Studio"": """",
-        ""Edicao"": """",
-        ""Descricao"": """",
-        ""Disponibilidade"": true || false,
-        ""Plataformas"": """"
-        }}";
+        try{
+                Console.WriteLine("Requisitando...");
+                var client = new OpenAI_API.OpenAIAPI(Environment.GetEnvironmentVariable("OPENAI_KEY"));
+                var chat = client.Chat.CreateConversation();
+                string prompt = $@"Para o jogo ""{titulo}"", preencha os campos adequadamente e retorne apenas o arquivo JSON preenchido. Descricao: Resuma o jogo de forma divertida e breve. Não repita informações de outros campos:
+                {{
+                ""Genero"": """",
+                ""Studio"": """",
+                ""Edicao"": """",
+                ""Descricao"": """",
+                ""Disponibilidade"": true || false,
+                ""Plataformas"": """"
+                }}";
 
-        chat.AppendSystemMessage(prompt);
-        string resposta = await chat.GetResponseFromChatbotAsync();
-        Console.WriteLine("Resposta do ChatGPT: " + resposta);
-        return resposta;    
+                chat.AppendSystemMessage(prompt);
+                string resposta = await chat.GetResponseFromChatbotAsync();
+                //Console.WriteLine("Resposta do ChatGPT: " + resposta);
+                Console.WriteLine("Feito!");
+                return resposta;                    
+        }catch (System.Exception){
+                Console.WriteLine("Erro na API - Tarefa Cancelada - Pressione Qualquer Tecla");
+                Console.ReadKey();
+                return "";
+        }
+
         }
 
 }
